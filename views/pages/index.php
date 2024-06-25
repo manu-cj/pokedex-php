5b2cd015ddcaa817db55b5027ca3844d5bf25bc9
Arnaud Van Acker <arnaudvanacker@yahoo.fr>
2024-06-25 14:09:00 +0200

    index user

--- views/pages/index.php
+++ views/pages/index.php
@@ -8,7 +8,7 @@ require_once __DIR__ . '../../partials/header.php';
 
 <main>
 <h1>Pokedex - Homepage</h1>
-<p>Hello <strong><?php echo $user['name'] ?></p></strong>
+<p>Hello <strong><?php echo $_SESSION["user"] ?></p></strong>
 <section class="pkmn-section">
     <?php
     require('./model/allpkmnManager.php');
