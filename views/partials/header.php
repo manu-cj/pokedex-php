224c74fae27f9c5fb5e7173de49972fd01d2f8c6
Iliess <m.iliess.abdelmadjid@gmail.com>
2024-06-26 13:55:22 +0200

    update

--- views/partials/header.php
+++ views/partials/header.php
@@ -5,13 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo htmlspecialchars($title); ?> - Pokedex</title>
-    <link href="./../../public/css/style.css" type="text/css" rel="stylesheet">
+    <link href="http://localhost/Becode/pokedex-php/public/css/style.css" type="text/css" rel="stylesheet">
     <link rel="icon" type="image/png" sizes="32x32" href="../../public/img/pokedex-icon.png">
 </head>
 
 <body>
 <header>
-<img class="pokemon-logo" src="../../public/img/pokemon-logo.png" alt="Pokemon logo">
+<img class="pokemon-logo" src="http://localhost/Becode/pokedex-php/public/img/pokemon-logo.png" alt="Pokemon logo">
     <div class="header-container">
         <div class="logo">            
             <h1><a class="pokedex-home" href="/">Pokedex</a></h1>
@@ -27,8 +27,8 @@
                     <a class="user-identification"href="profile.php"><!--RegisterManager.php???-->My account</a>
                     <a class="user-identification"href="logout.php">Logout</a>
                 <?php else: ?>
-                    <a class="user-identification"href="login.php">Login</a>
-                    <a class="user-identification"href="register.php">Register</a>
+                    <a class="user-identification"href="?c=login">Login</a>
+                    <a class="user-identification"href="?c=register">Register</a>
                 <?php endif; ?>
                    </nav>
     </div>
