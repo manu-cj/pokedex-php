56b77c2b9fd0fca7d0e79830ed5bc6e1efcaac7e
Tasha <nataliiamelnykova9@gmail.com>
2024-06-27 09:35:07 +0200

    footer

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -2,21 +2,25 @@
     <div class="footer-container">
         <div class="team">
             <div class="teamplayer">
-                <img src="../../public/img/footer/pkmn.svg" class="svg" alt="Pikachu face">
+                <a class="github" href="https://github.com/manu-cj" target="blanc" ><img src="../../public/img/footer/pkmn.svg" class="svg" alt="Pikachu face"></a>
                 <h6>Manu</h6>
             </div>
             <div class="teamplayer">
-                <img src="../../public/img/footer/pkmn.svg" class="svg" alt="Pikachu face">
+                <a class="github" href="https://github.com/Iliess-A" target="blanc" ><img src="../../public/img/footer/pkmn.svg" class="svg" alt="Pikachu face"></a>
                 <h6>Ilies</h6>
             </div>
             <div class="teamplayer">
-                <img src="../../public/img/footer/pkmn.svg" class="svg" alt="Pikachu face">
+                <a class="github" href="https://github.com/javadaller" target="blanc" ><img src="../../public/img/footer/pkmn.svg" class="svg" alt="Pikachu face"></a>
+                <h6>Arnaud</h6>
+            </div>
+            <div class="teamplayer">
+                <a class="github" href="https://github.com/NataliiaTasha" target="blanc" ><img src="../../public/img/footer/pkmn.svg" class="svg" alt="Pikachu face"></a>
                 <h6>Tasha</h6>
             </div>
         </div>
         <div class="contacts">
             <img src="../../public/img/footer/location.svg" class="svg location" alt="Location icon">
-            <p>Address</p>
+            <p>Square des Martyrs, 1 c/o A6K, 6000 Charleroi</p>
         </div>
     </div>
     <div class="footer-bottom">
